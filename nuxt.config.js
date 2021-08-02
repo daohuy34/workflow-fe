1adce0670142fe88138e35e7e8b878b18955b0f1
QuangHuy <daohuy34@gmail.com>
2021-08-02 16:29:32 +0700

    set target nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,8 +16,8 @@ export default {
         ],
         link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
     },
+    target: 'static',
     srcDir: 'source/client/',
-    // layouts: 'layouts/',
     // Global CSS: https://go.nuxtjs.dev/config-css
     css: [],
 
